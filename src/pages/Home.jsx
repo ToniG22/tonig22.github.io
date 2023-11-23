6e11cffe26dc861fa50f7f4006901463057db53a
LuisAlbrt02 <totocasillas26@gmail.com>
2023-11-23 12:58:50 +0000

    Updated Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 import EventsGrid from "./EventsGrid";
 
 const Home = () => {
-  
   const [events, setEvents] = useState([]);
 
   const reloadEvents = () => {
@@ -16,7 +15,7 @@ const Home = () => {
     reloadEvents();
   }, []);
 
-  console.log(events)
+  console.log(events);
 
   const today = new Date(); // Get today's date
   const startOfWeek = new Date(today);
@@ -32,63 +31,69 @@ const Home = () => {
   const weeklyEvents = events.filter((event) => {
     const eventDate = new Date(event.endDate); // Assuming event.dates[0] is in "YYYY-MM-DD" format
     // Compare the event date against the entire week
-    return eventDate >= startOfWeek && eventDate <= endOfWeek || {};
+    return (eventDate >= startOfWeek && eventDate <= endOfWeek) || {};
   });
   
 
   return (
     <div>
       <header>
-        <h1>
-          <div className='Home-Float'> 
-            <p className='H1-home'>MELHORES EVENTOS</p>
-            <p className='H1-home'>CULTURAIS NA ILHA DA</p>
-          </div>
-          <div className='H1-madeira'> 
-            MADEIRA
-          </div>
+        <h1>       
+            <div className="H1-home">
+              <div>Melhores eventos</div> 
+              <div className="MiddleH1Home">culturais na ilha da</div> 
+              <div className="H1-madeira"> Madeira </div>
+            </div>
         </h1>
       </header>
 
       <main>
         <section>
-            <div className='H2-block'>
-            <h2 className='H2-home'>Destaques</h2>
+          <div className="H2-block">
+            <h2 className="H2-home">Destaques</h2>
             <EventsGrid events={weeklyEvents} />
-            </div>
+          </div>
         </section>
 
         <section>
-          <div>
-          <NavLink to="festivais"><button className='Button-home' ><b>Festivais</b></button></NavLink>
-          </div>
-          <div>
-            <p className='Home-Box'>
-              Festivais são eventos culturais que abrangem uma ampla gama de temas, como música, arte,
-              comida, religião e muito mais. Eles podem variar em duração, desde festivais de um dia 
-              até eventos que duram semanas. Os festivais atraem públicos diversificados e oferecem uma
-              variedade de atividades culturais e artísticas, tornando-se celebrações de grande alcance
-              e importância cultural.
+          <div className="HomeDescription">
+            <NavLink to="festivais">
+              <button className="Button-home">
+                <b>Festivais</b>
+              </button>
+            </NavLink>
+            <p className="Home-Box">
+              Festivais são eventos culturais que abrangem uma ampla gama de
+              temas, como música, arte, comida, religião e muito mais. Eles
+              podem variar em duração, desde festivais de um dia até eventos que
+              duram semanas.
             </p>
           </div>
-          <div>
-            <NavLink to="arraiais"><button className='Button-home'><b>Arraiais</b></button></NavLink>
-          </div>
-          <div>
-            <p className='Home-Box'>
-              Arraiais são festas tradicionais. Estas celebrações são marcadas por danças folclóricas 
-              e comidas típicas da zona. Geralmente realizados em áreas rurais, os arraiais são festas
-              curtas que unem devoção religiosa e cultura popular, proporcionando momentos de alegria 
-              para as comunidades locais.
+
+          <div className="HomeDescription">
+            <NavLink to="arraiais">
+              <button className="Button-home">
+                <b>Arraiais</b>
+              </button>
+            </NavLink>
+            <p className="Home-Box">
+              Arraiais são festas tradicionais. Estas celebrações são marcadas
+              por danças folclóricas e comidas típicas da zona. Estes são
+              considerados festas curtas que unem devoção religiosa e cultura
+              popular, proporcionando momentos de alegria para as comunidades
+              locais.
             </p>
           </div>
-          <div>
-            <NavLink to="mapa"><button className='Button-home'><b>Mapa</b></button></NavLink>
-          </div>
-          <div>
-            <p className='Home-Box'>
-              Aqui poderá ver os eventos que estão a decorrer na ilha da Madeira, seja arraiais ou festivais. 
-              Poderá encontrar os eventos que estão a decorrer na freguesia que escolheu.
+          <div className="HomeDescription">
+            <NavLink to="mapa">
+              <button className="Button-home">
+                <b>Mapa</b>
+              </button>
+            </NavLink>
+            <p className="Home-Box">
+              Aqui poderá ver os eventos que estão a decorrer na ilha da
+              Madeira, seja arraiais ou festivais. Poderá encontrar os eventos
+              que estão a decorrer na freguesia que escolheu.
             </p>
           </div>
         </section>
@@ -96,6 +101,5 @@ const Home = () => {
     </div>
   );
 };
-
 
 export default Home;
